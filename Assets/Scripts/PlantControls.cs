60f7c928d6a8706c2a2cb9fcbb488a332c80d541
João Inácio <egnawake@gmail.com>
2023-02-05 19:17:16 +0000

    Show plant cost and value

--- Assets/Scripts/PlantControls.cs
+++ Assets/Scripts/PlantControls.cs
@@ -4,9 +4,13 @@ using TMPro;
 public class PlantControls : MonoBehaviour
 {
     [SerializeField] private TMP_Text selectedPlantNameText;
+    [SerializeField] private TMP_Text selectedPlantCostText;
+    [SerializeField] private TMP_Text selectedPlantValueText;
 
-    public void SetSelectedPlant(string name)
+    public void SetSelectedPlant(Plant plant)
     {
-        selectedPlantNameText.text = name;
+        selectedPlantNameText.text = plant.Name;
+        selectedPlantCostText.text = $"Cost: {plant.Cost.ToString()}";
+        selectedPlantValueText.text = $"Value: {plant.Value.ToString()}";
     }
 }
